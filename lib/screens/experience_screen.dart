e825de1015148f31750340cbd5d33f03090549aa
erginushi <erginushi9@gmail.com>
2022-10-22 17:11:00 +0200

    latest

--- lib/screens/experience_screen.dart
+++ lib/screens/experience_screen.dart
@@ -23,101 +23,102 @@ class _ExperienceScreenState extends State<ExperienceScreen> {
       child: SizedBox(
         height: sh,
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Hero(
-                tag: widget.data['id'],
-                child: Container(
-                  child: Stack(
-                    clipBehavior: Clip.antiAlias,
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Image.network(
-                          'https://albania360.com/wp-content/uploads/2022/06/286705471_5226925277344994_9150394650843000115_n-e1655302581295.jpg'),
-                      TextButton(
-                        style: ButtonStyle(
-                          overlayColor:
-                              MaterialStateProperty.all(Colors.transparent),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            favourited = !favourited;
-                          });
-                        },
-                        child: new ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(60)),
-                          child: new BackdropFilter(
-                            filter: new ImageFilter.blur(
-                                sigmaX: 10.0, sigmaY: 10.0),
-                            child: new Container(
-                              width: 60.0,
-                              height: 60.0,
-                              decoration: new BoxDecoration(
-                                  color: Colors.grey.shade200.withOpacity(0.5)),
-                              child: new Center(
-                                child: new Icon(
-                                  favourited
-                                      ? Icons.favorite
-                                      : Icons.favorite_border,
-                                  size: 33,
-                                  color: favourited
-                                      ? Colors.redAccent
-                                      : Color.fromRGBO(0, 0, 0, 0.6),
-                                ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Hero(
+              tag: widget.data['id'],
+              child: Container(
+                child: Stack(
+                  clipBehavior: Clip.antiAlias,
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    Image.network(
+                        'https://albania360.com/wp-content/uploads/2022/06/286705471_5226925277344994_9150394650843000115_n-e1655302581295.jpg'),
+                    TextButton(
+                      style: ButtonStyle(
+                        overlayColor:
+                            MaterialStateProperty.all(Colors.transparent),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          favourited = !favourited;
+                        });
+                      },
+                      child: new ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(60)),
+                        child: new BackdropFilter(
+                          filter:
+                              new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                          child: new Container(
+                            width: 60.0,
+                            height: 60.0,
+                            decoration: new BoxDecoration(
+                                color: Colors.grey.shade200.withOpacity(0.5)),
+                            child: new Center(
+                              child: new Icon(
+                                favourited
+                                    ? Icons.favorite
+                                    : Icons.favorite_border,
+                                size: 33,
+                                color: favourited
+                                    ? Colors.redAccent
+                                    : Color.fromRGBO(0, 0, 0, 0.6),
                               ),
                             ),
                           ),
                         ),
-                      )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 24, top: 16, right: 24, bottom: 4),
+              child: Text(
+                'Petrela ZIP LINE',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 24, top: 4, right: 24, bottom: 4),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                child: Container(
+                  width: sw,
+                  height: 200,
+                  child: FlutterMap(
+                    mapController: MapController(),
+                    options: MapOptions(),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      ),
                     ],
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 24, top: 16, right: 24, bottom: 4),
-                child: Text(
-                  'Petrela ZIP LINE',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 24, top: 4, right: 24, bottom: 4),
-                child: Text(
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 18),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                  child: Container(
-                    width: sw,
-                    height: 200,
-                    child: FlutterMap(
-                      mapController: MapController(),
-                      options: MapOptions(),
-                      children: [
-                        TileLayer(
-                          urlTemplate:
-                              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          userAgentPackageName: 'com.example.app',
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ]),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/experience_card.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -11,12 +13,57 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       child: Column(
         children: [
-          Row(
-            children: const [
-              Text('Tirana, Albania'),
-            ],
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                const Text(
+                  'Tirana, ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(
+                  'Albania',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: const [
+                Text(
+                  'Experiences near you',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 20,
+            child: ListView.separated(
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(vertical: 10),
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(
+                  height: 30,
+                );
+              },
+              itemBuilder: (BuildContext context, int index) {
+                String heroTag = 'tag_$index';
+                return Hero(
+                  tag: heroTag,
+                  child: ExperinceCard(heroTag: heroTag),
+                );
+              },
+              itemCount: 5,
+            ),
           ),
         ],
       ),
