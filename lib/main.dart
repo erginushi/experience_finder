bf2d4415e196aa64ad2bdbe1c1c5a0472dee475a
erginushi <erginushi9@gmail.com>
2022-10-22 15:15:00 +0200

    navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:epxerience_finder/screens/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,14 +33,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: HomeScreen(),
+      bottomNavigationBar: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: const [
+          HomeScreen(),
+          Text('123'),
+          Text('123'),
+          Text('123'),
+          Text('123'),
+        ],
+        items: [
+          PersistentBottomNavBarItem(
+            icon: Icon(CupertinoIcons.globe),
+            title: ("Experiences"),
+            activeColorPrimary: CupertinoColors.activeBlue,
+            inactiveColorPrimary: CupertinoColors.systemGrey,
+          ),
+          PersistentBottomNavBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: ("Search"),
+            activeColorPrimary: CupertinoColors.activeBlue,
+            inactiveColorPrimary: CupertinoColors.systemGrey,
+          ),
+          PersistentBottomNavBarItem(
+            icon: const Icon(
+              CupertinoIcons.add,
+              color: Colors.white,
+            ),
+            // title: ("Settings"),
+            activeColorPrimary: CupertinoColors.activeBlue,
+            inactiveColorPrimary: CupertinoColors.systemGrey,
+          ),
+          PersistentBottomNavBarItem(
+            icon: Icon(CupertinoIcons.heart),
+            title: ("Favourites"),
+            activeColorPrimary: CupertinoColors.activeBlue,
+            inactiveColorPrimary: CupertinoColors.systemGrey,
+          ),
+          PersistentBottomNavBarItem(
+            icon: Icon(CupertinoIcons.profile_circled),
+            title: ("Profile"),
+            activeColorPrimary: CupertinoColors.activeBlue,
+            inactiveColorPrimary: CupertinoColors.systemGrey,
+          ),
+        ],
+        confineInSafeArea: true,
+        backgroundColor: Colors.white, // Default is Colors.white.
+        handleAndroidBackButtonPress: true, // Default is true.
+        resizeToAvoidBottomInset:
+            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+        stateManagement: true, // Default is true.
+        hideNavigationBarWhenKeyboardShows:
+            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: ItemAnimationProperties(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimation(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+            NavBarStyle.style16, // Choose the nav bar style with this property.
       ),
     );
   }
